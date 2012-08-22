ef1dbc62ea95713a0d02d7935cc9ba1cec3c3e0e
Alex LaFroscia <alex.lafroscia@gmail.com>
2012-08-22 15:27:19 -0400

    Fixed user scalability on mobile devices

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
 	<link rel="apple-touch-icon" href="img/Apple-Icon.png" />
 	<link rel="apple-touch-icon" sizes="114x114" href="img/Apple-Icon-Retina.png" />
 	
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0 user-scalable=no>
 	<meta http-equiv="X-UA-Compatible" content="chrome=1">
 
 
