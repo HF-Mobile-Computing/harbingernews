cb76d1241be8a610c384ca2d061375d0bf8af408
alexlafroscia <alex.lafroscia@gmail.com>
2012-09-11 09:38:55 -0400

    Removed Hammer

---
+++ arts/index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+
+	<title>HHS - Arts</title>
+	
+	<script src="/js/jquery-1.8.0.min.js"></script>
+
+	<link rel="stylesheet" href="/~alex/Harbinger2/css/all.css" type="text/css">
+	
+	<style type="text/css">
+
+		body {
+			margin: 0;
+			background-image:url(/~alex/Harbinger2/Background.png);
+		}
+		
+		li#arts {
+			background-color: #629b63 !important;
+		}
+
+	</style>
+
+</head>
+
+<body>
+
+	<div class="content">
+	
+		<!--#include virtual="/~alex/Harbinger2/header.shtml"-->
+		
+		This is a test.
+	
+	
+	<div class="footer_spacer"></div>
+	
+	</div>
+	
+	<div class="footer">
+	
+		<!--#include virtual="/~alex/Harbinger2/footer.shtml" -->
+		
+		<div class="container" id="edited_by">
+			<div class="span6 offset6">
+				<span id="edited_by">Last edited by Alex LaFroscia on August 22, 2012.</span>
+			</div><!-- end "span6" -->
+		</div><!-- end "edited_by" -->
+
+	</div>
+
+
+</body>
+
+</html>
