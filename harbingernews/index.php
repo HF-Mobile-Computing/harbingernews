4d27d46fa4ccfebeeb2ea2beffe9c36afa8b79e9
alexlafroscia <alex.lafroscia@gmail.com>
2012-10-18 13:10:25 -0400

    Added Google Analytics Information

--- harbingernews/index.php
+++ harbingernews/index.php
@@ -62,6 +62,17 @@
 	$success3 = $feed3->init();
 	$feed3->handle_content_type();
 	?>
+	
+	<script type="text/javascript">
+		var _gaq = _gaq || [];
+		_gaq.push(['_setAccount', 'UA-35687930-1']);
+		_gaq.push(['_trackPageview']);
+		(function() {
+			var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+			ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+			var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+		})();
+	</script>
 
 </head>
 
@@ -196,7 +207,7 @@
 						</div><!-- end "sp_results"-->
 			
 					</div><!-- end "important"-->
-
+fs
 				</div><!-- end "span6" -->
 				
 				<div class="span4">
