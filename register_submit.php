eea75e16911b72aaf63becd772b7fc69d63ba6eb
alexlafroscia <alex.lafroscia@gmail.com>
2012-12-23 14:02:44 -0500

    Added check to make sure that there are no other users with the same name when registering a new user

--- register_submit.php
+++ register_submit.php
@@ -19,7 +19,15 @@ $hash = hash('sha256', $salt . $hash);
 include 'datalogin.php';
 // sanitize username
 $username = mysql_real_escape_string($username);
-$query = "INSERT INTO users ( username, password, salt) VALUES ('$username' , '$hash' , '$salt' );";
-mysql_query($query);
-mysql_close();
-header('Location: login_form.php');
+$nameCheck = "SELECT * FROM users WHERE username='$username';";
+$check = mysql_query($nameCheck);
+if(mysql_num_rows($check) > 0)
+{
+	echo "Sorry, that username has been taken already.  Please try a new username.";
+} else {
+	$query = "INSERT INTO users ( username, password, salt) VALUES ('$username' , '$hash' , '$salt' );";
+	mysql_query($query);
+	mysql_close();
+	header('Location: login.php');
+}
+?>
