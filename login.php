9459b88e9236ac62622b715724da7470bc597a72
alexlafroscia <alex.lafroscia@gmail.com>
2012-12-23 17:04:42 -0500

    Added the action to point to an absolute URL

--- login.php
+++ login.php
@@ -4,7 +4,7 @@
 	<title></title>
 </head>
 <body>
-	<form name="login" action="login_submit.php" method="post">
+	<form name="login" action="/harbingernews/login_submit.php" method="post">
    		Username: <input type="text" name="username" /><br />
    		Password: <input type="password" name="password" /><br />
    		<input type="submit" value="Login" />
