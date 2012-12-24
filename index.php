c76400233af29523348b5656dda6467cd7db7dfb
alexlafroscia <alex.lafroscia@gmail.com>
2012-12-24 11:48:27 -0500

    Installed CodeIgniter
    - Added CodeIgniter files
    - Moved old files to an "Old Files" folder

--- index.php
+++ index.php
@@ -1,202 +1,205 @@
 <?php
-session_start();
-include 'sessions.php';
-$starttime = explode(' ', microtime());
-$starttime = $starttime[1] + $starttime[0];
-include_once('autoloader.php');
-include_once('idn/idna_convert.class.php');
-$feed = new SimplePie();
-$feed->set_feed_url('http://sites.google.com/site/harborfieldshs/home/posts.xml');
-$feed->enable_cache(false);
-$success = $feed->init();
-$feed->handle_content_type();
-$feed2 = new SimplePie();
-$feed2->set_feed_url('http://hfrecent.blogspot.com/feeds/posts/default');
-$feed2->enable_cache(false);
-$success2 = $feed2->init();
-$feed2->handle_content_type();
-$feed3 = new SimplePie();
-$feed3->set_feed_url('http://hfupcoming.blogspot.com/feeds/posts/default');
-$feed3->enable_cache(false);
-$success3 = $feed3->init();
-$feed3->handle_content_type();
-?>
-<!DOCTYPE html>
-<html>
 
-<head>
+/*
+ *---------------------------------------------------------------
+ * APPLICATION ENVIRONMENT
+ *---------------------------------------------------------------
+ *
+ * You can load different configurations depending on your
+ * current environment. Setting the environment also influences
+ * things like logging and error reporting.
+ *
+ * This can be set to anything, but default usage is:
+ *
+ *     development
+ *     testing
+ *     production
+ *
+ * NOTE: If you change these, also change the error_reporting() code below
+ *
+ */
+	define('ENVIRONMENT', 'development');
+/*
+ *---------------------------------------------------------------
+ * ERROR REPORTING
+ *---------------------------------------------------------------
+ *
+ * Different environments will require different levels of error reporting.
+ * By default development will show errors but testing and live will hide them.
+ */
 
-	<title>The Harbinger Online</title>
+if (defined('ENVIRONMENT'))
+{
+	switch (ENVIRONMENT)
+	{
+		case 'development':
+			error_reporting(E_ALL);
+		break;
 	
-	<!-- This is a test -->
-	
-	<!-- enable webapp on iPhone -->
-	<meta name="apple-mobile-web-app-capable" content="yes" />
-	<link rel="apple-touch-icon" href="img/Apple-Icon.png" />
-	<link rel="apple-touch-icon" sizes="114x114" href="img/Apple-Icon-Retina.png" />
-	<link rel="icon" type="image/png" href="/harbingernews/img/favicon.ico">
-	
-	<meta name="viewport" content="width=device-width, initial-scale=1.0 user-scalable=no">
-	<meta http-equiv="X-UA-Compatible" content="chrome=1">
-	
-	<link rel="stylesheet" href="/harbingernews/css/bootstrap.css" type="text/css">
-	<link rel="stylesheet" href="/harbingernews/css/bootstrap-responsive.css" type="text/css">
-	<link rel="stylesheet" href="/harbingernews/css/jquery.fancybox.css" type="text/css">
-	<link rel="stylesheet" href="/harbingernews/css/homepage.css" type="text/css">
-	
-	<script src="js/jquery-1.8.0.min.js"></script>
-	<script src="js/bootstrap.js" type="text/javascript"></script>
-	<script src="js/jquery.news_stories.js" type="text/javascript"></script>
-	<script src="js/jquery.fancybox.pack.js" type="text/javascript"></script>
+		case 'testing':
+		case 'production':
+			error_reporting(0);
+		break;
 
-	<script src="http://ajax.googleapis.com/ajax/libs/chrome-frame/1/CFInstall.min.js"></script>
+		default:
+			exit('The application environment is not set correctly.');
+	}
+}
 
-	<script type="text/javascript">
-		
-		// Fancybox
-		$(function() {
-			$('.fancy').fancybox({
-				'hideOnContentClick' : false,
-				'openEffect' : 'fade',
-			});
-			// Set the Home tab to 'active'
-			$('#nav_home').addClass('active');
-		});
-	</script>
-	
-	<!-- Google Analytics -->
-	<script type="text/javascript">
-		var _gaq = _gaq || [];
-		_gaq.push(['_setAccount', 'UA-35687930-1']);
-		_gaq.push(['_trackPageview']);
-		(function() {
-	    	var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-	    	ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-	    	var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-	    })();
-	</script>
+/*
+ *---------------------------------------------------------------
+ * SYSTEM FOLDER NAME
+ *---------------------------------------------------------------
+ *
+ * This variable must contain the name of your "system" folder.
+ * Include the path if the folder is not in the same  directory
+ * as this file.
+ *
+ */
+	$system_path = 'system';
 
-</head>
+/*
+ *---------------------------------------------------------------
+ * APPLICATION FOLDER NAME
+ *---------------------------------------------------------------
+ *
+ * If you want this front controller to use a different "application"
+ * folder then the default one you can set its name here. The folder
+ * can also be renamed or relocated anywhere on your server.  If
+ * you do, use a full server path. For more info please see the user guide:
+ * http://codeigniter.com/user_guide/general/managing_apps.html
+ *
+ * NO TRAILING SLASH!
+ *
+ */
+	$application_folder = 'application';
 
-<body>
-	
-	<div id="content">
+/*
+ * --------------------------------------------------------------------
+ * DEFAULT CONTROLLER
+ * --------------------------------------------------------------------
+ *
+ * Normally you will set your default controller in the routes.php file.
+ * You can, however, force a custom routing by hard-coding a
+ * specific controller class/function here.  For most applications, you
+ * WILL NOT set your routing here, but it's an option for those
+ * special instances where you might want to override the standard
+ * routing in a specific front controller that shares a common CI installation.
+ *
+ * IMPORTANT:  If you set the routing here, NO OTHER controller will be
+ * callable. In essence, this preference limits your application to ONE
+ * specific controller.  Leave the function name blank if you need
+ * to call functions dynamically via the URI.
+ *
+ * Un-comment the $routing array below to use this feature
+ *
+ */
+	// The directory name, relative to the "controllers" folder.  Leave blank
+	// if your controller is not in a sub-folder within the "controllers" folder
+	// $routing['directory'] = '';
 
-		<?php include('tools/header.php') ?>
-			
-		<div class="container" id="all">
-			<div class="container-fluid" id="stuff">
-				<div class="row-fluid"><!-- header -->
-					<div class="span12" id="header">
-						<h1>Harborfields High School</h1>
-					</div>
-				</div><!-- end header -->
-				<div class="row-fluid" id="announcements">
-					<h3>Today's Announcements</h3>
-					<?php
-						if ($feed->error())
-						{
-							echo '<div class="sp_errors">' . "\r\n";
-								echo '<p>' . htmlspecialchars($feed->error()) . "</p>\r\n";
-							echo '</div>' . "\r\n";
-						}
-					?>
-					<div id="sp_results">
-						<?php if ($success): ?>
-							<?php foreach($feed->get_items(0, 1) as $item): ?>
-								<div class="chunk">			
-									<?php echo $item->get_content(); ?>
-								</div><!-- end "chunk" -->
-							<?php endforeach; ?>
-						<?php endif; ?>
-					</div><!-- end "sp_results"-->
-				</div>
-				<div class="row-fluid" id="postits">
-					<a href="http://www.yearbookforever.com/ssDeepLink.aspx?sid=1-23J-32132&dest=BAYB"><div class="span4" id="yearbook">
-						<img src="/harbingernews/img/ssDeepLink.png" alt="Buy a Yearbook" />
-					</div></a>
-					<div class="span4" id="snowflake">
-						<h4>Support LB Storm Clean-Up</h4>
-						<p>Buy a Secret Snowflake from Leadership</p>
-					</div>
-					<div class="span4" id="playfest">
-						<h4>Join playfest!</h4>
-						<p>Contact your grade's government for more information about how you can get involved</p>
-					</div>
-				</div>
-				<div class="row-fluid" id="events">
-					<div class="span6" id="upcoming">
-						<h3>Upcoming Events</h3>
-						<?php
-							if ($feed3->error())
-							{
-								echo '<div class="sp_errors">' . "\r\n";
-									echo '<p>' . htmlspecialchars($feed3->error()) . "</p>\r\n";
-								echo '</div>' . "\r\n";
-							}
-						?>
-						<div id="sp_results">
-							<?php if ($success3): ?>
-								<?php foreach($feed3->get_items(0, 1) as $item): ?>
-									<div class="chunk">			
-										<?php echo $item->get_content(); ?>
-									</div><!-- end "chunk" -->
-								<?php endforeach; ?>
-							<?php endif; ?>
-						</div><!-- end "sp_results"-->
+	// The controller class file name.  Example:  Mycontroller
+	// $routing['controller'] = '';
 
-					</div>
-					<div class="span6" id="recent">
-						<h3>Recent Events</h3>
-						<?php
-							if ($feed2->error())
-							{
-								echo '<div class="sp_errors">' . "\r\n";
-									echo '<p>' . htmlspecialchars($feed->error()) . "</p>\r\n";
-								echo '</div>' . "\r\n";
-							}
-						?>
-						<div id="sp_results">
-							<?php if ($success2): ?>
-								<?php foreach($feed2->get_items(0, 1) as $item): ?>
-									<div class="chunk">			
-										<?php echo $item->get_content(); ?>
-									</div><!-- end "chunk" -->
-								<?php endforeach; ?>
-							<?php endif; ?>
-						</div><!-- end "sp_results"-->
-					</div>
-				</div>
-			</div>
-			<div class="well" id="sidebar">
-				<a href="http://www.yearbookforever.com/ssDeepLink.aspx?sid=1-23J-32132&dest=BAYB"><div id="yearbook">
-					<img src="/harbingernews/img/ssDeepLink.png" alt="Buy a Yearbook" />
-				</div></a>
-				<div id="snowflake">
-					<h4>Support LB Storm Clean-Up</h4>
-					<p>Buy a Secret Snowflake from Leadership</p>
-				</div>
-				<div id="playfest">
-					<h4>Join playfest!</h4>
-					<p>Contact your grade's government for more information about how you can get involved</p>
-				</div>
-			</div>
-		</div>		
-		<div id="footer_spacer"></div>
-		
-	</div><!-- end "content"-->
-	
-	<div id="footer">
-	
-		<?php include('tools/footer.php') ?>
-		
-		<div class="container">
-			<div class="pull-right" style="margin-top: 20px;">
-				<?php echo "This page was last updated on " . date ("F d Y.", getlastmod()); ?>
-			</div>
-		</div>
-		
-	</div> <!-- end "footer" -->
+	// The controller function you wish to be called.
+	// $routing['function']	= '';
 
-</body>
-</html>
+
+/*
+ * -------------------------------------------------------------------
+ *  CUSTOM CONFIG VALUES
+ * -------------------------------------------------------------------
+ *
+ * The $assign_to_config array below will be passed dynamically to the
+ * config class when initialized. This allows you to set custom config
+ * items or override any default config values found in the config.php file.
+ * This can be handy as it permits you to share one application between
+ * multiple front controller files, with each file containing different
+ * config values.
+ *
+ * Un-comment the $assign_to_config array below to use this feature
+ *
+ */
+	// $assign_to_config['name_of_config_item'] = 'value of config item';
+
+
+
+// --------------------------------------------------------------------
+// END OF USER CONFIGURABLE SETTINGS.  DO NOT EDIT BELOW THIS LINE
+// --------------------------------------------------------------------
+
+/*
+ * ---------------------------------------------------------------
+ *  Resolve the system path for increased reliability
+ * ---------------------------------------------------------------
+ */
+
+	// Set the current directory correctly for CLI requests
+	if (defined('STDIN'))
+	{
+		chdir(dirname(__FILE__));
+	}
+
+	if (realpath($system_path) !== FALSE)
+	{
+		$system_path = realpath($system_path).'/';
+	}
+
+	// ensure there's a trailing slash
+	$system_path = rtrim($system_path, '/').'/';
+
+	// Is the system path correct?
+	if ( ! is_dir($system_path))
+	{
+		exit("Your system folder path does not appear to be set correctly. Please open the following file and correct this: ".pathinfo(__FILE__, PATHINFO_BASENAME));
+	}
+
+/*
+ * -------------------------------------------------------------------
+ *  Now that we know the path, set the main path constants
+ * -------------------------------------------------------------------
+ */
+	// The name of THIS file
+	define('SELF', pathinfo(__FILE__, PATHINFO_BASENAME));
+
+	// The PHP file extension
+	// this global constant is deprecated.
+	define('EXT', '.php');
+
+	// Path to the system folder
+	define('BASEPATH', str_replace("\\", "/", $system_path));
+
+	// Path to the front controller (this file)
+	define('FCPATH', str_replace(SELF, '', __FILE__));
+
+	// Name of the "system folder"
+	define('SYSDIR', trim(strrchr(trim(BASEPATH, '/'), '/'), '/'));
+
+
+	// The path to the "application" folder
+	if (is_dir($application_folder))
+	{
+		define('APPPATH', $application_folder.'/');
+	}
+	else
+	{
+		if ( ! is_dir(BASEPATH.$application_folder.'/'))
+		{
+			exit("Your application folder path does not appear to be set correctly. Please open the following file and correct this: ".SELF);
+		}
+
+		define('APPPATH', BASEPATH.$application_folder.'/');
+	}
+
+/*
+ * --------------------------------------------------------------------
+ * LOAD THE BOOTSTRAP FILE
+ * --------------------------------------------------------------------
+ *
+ * And away we go...
+ *
+ */
+require_once BASEPATH.'core/CodeIgniter.php';
+
+/* End of file index.php */
+/* Location: ./index.php */
