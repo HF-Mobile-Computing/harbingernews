8887b411c7ca0d9bdf144d17b5cbf3ef1900a737
Brian <bkelley620@gmail.com>
2013-03-19 15:14:08 -0400

    Made initial page for computer club

---
+++ clubs/Mobile Computer Club/Mobile Computer Club.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Mobile Computer Club</title>
+	
+	<meta charset="utf-8">
+
+	<link rel="stylesheet" href="/harbingernews/css/bootstrap.css" type="text/css">
+	<link rel="stylesheet" href="/harbingernews/css/bootstrap-responsive.css" type="text/css">
+	<link rel="stylesheet" href="/harbingernews/css/all.css" type="text/css">
+	<link rel="stylesheet" href="/harbingernews/css/clubs.css" type="text/css">
+
+	<script type="text/javascript" src="/harbingernews/js/jquery-1.8.0.min.js"></script>
+	<script type="text/javascript" src="/harbingernews/js/bootstrap.min.js"></script>
+		
+	
+	
+</head>
+<body>
+	<div id = "content" style ="">
+		<!--#include virtual="/harbingernews/tools/header.shtml"-->
+		<?php include('../header2.shtml') ?>
+		
+		<div id="info" class="container">
+			<div class="row">
+				<div class="span8" id="reminders" >
+					<!--
+					When enabled, (^ get rid of display:none;) shows a box to be used for important reminders such as reschedulings or cancellations. Maybe more. 
+					-->
+					<div class="alert alert-error">
+  						<button type="button" class="close" data-dismiss="alert">&times;</button>
+  						<h3>Important!</h3>
+					</div>
+				
+				</div>
+				<div id="upcoming_events" style = "display: none;">
+					<h4>Upcoming Events</h4>
+					<ul>
+						<li>Event One</li>
+						<li>Event Two</li>
+						<li>Event Three</li>
+						<li>Event Four</li>
+					</ul>
+					
+				</div>		
+		
+			</div> 
+			<div class="row">
+				<div class = "about">
+					<h4 class = "offset1">About The Mobile Computing Club</h4>
+					<p>Advisor: Mr. Schaeffer</p> 
+					<p>Room: 405</p>
+					<p>Phone and app development</p>
+					<p>Link to the website: https://sites.google.com/site/mobilecomputingclubhhs/</p>
+					<br/>
+					<p>This club meets on Tuesdays</p>
+
+				</div>
+			
+			</div><!-- end "row" -->
+			
+			<br />
+			
+			<div class="row" style = "display:none;">
+				<div class="span4" id="photo_holder">
+					<em>Photo Holder</em>
+				</div>
+				<div class="span4" id="photo_holder">
+					<em>Photo Holder</em>
+				</div>
+				
+				<div class="span4" id="photo_holder">
+					<em>Photo Holder</em>
+				</div>
+			</div><!-- end "row" -->
+		</div>
+	
+	
+	<div id="footer_spacer"></div>
+	</div><!-- end "content" -->
+	
+	<div id="footer">
+		<?php include('../../tools/footer.shtml') ?>
+		
+		<div class="container">
+			<div class="pull-right">
+					This page was last updated by Brian Kelley on January 15, 2013.
+			</div>
+		</div>
+	
+	</div>
+	
+</body>
+</html>
