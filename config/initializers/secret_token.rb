b0d318e611e72f32a3633d91372a5dda0a7d1f9c
alexlafroscia <alex.lafroscia@gmail.com>
2013-03-24 18:19:27 -0400

    Officially a Rails app now! Woohoo!

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Harbingernews::Application.config.secret_token = '8f7676cfecc68420d78b873929c32916ed33e2f0f310c25d19c691f2c18fe95b2999571e7d37241577aadef92e117987a682cda9abc10db9df9a64f9ff9b76ae'
