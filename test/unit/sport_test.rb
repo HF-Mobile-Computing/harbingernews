b14ba7999f7174e039ded70bebbd255f419adddb
alexlafroscia <alex.lafroscia@gmail.com>
2013-03-27 23:40:01 -0400

    Added schema information

--- test/unit/sport_test.rb
+++ test/unit/sport_test.rb
@@ -5,3 +5,18 @@ class SportTest < ActiveSupport::TestCase
   #   assert true
   # end
 end
+# == Schema Information
+#
+# Table name: sports
+#
+#  id         :integer         not null, primary key
+#  slug       :string(255)
+#  title      :string(255)
+#  content    :text
+#  season     :string(255)
+#  photo_path :string(255)
+#  created_at :datetime        not null
+#  updated_at :datetime        not null
+#  alert      :text
+#
+
