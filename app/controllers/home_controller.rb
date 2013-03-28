9fad44ccb0adc9564a7368400544926e4971b55c
alexlafroscia <alex.lafroscia@gmail.com>
2013-03-28 19:35:52 -0400

    Got the home page dynamically showing the announcements

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,15 @@
 class HomeController < ApplicationController
+  
+  before_filter :set_vars
+  
   def index
-    render layout: false  
+    render layout: false
+  end
+  
+  protected
+    
+    def set_vars
+      @announcement = Announcement.last
+      @hello = "hello"
   end
 end
