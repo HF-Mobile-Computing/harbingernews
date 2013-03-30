e62ec5dd9ddbe905f3ce7e5cb20679bf0f17072b
alexlafroscia <alex.lafroscia@gmail.com>
2013-03-29 22:18:51 -0400

    More trial and error to get the Production server working.
    Still having issues with asset compiling.  No idea what's going on.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -9,7 +9,7 @@ Harbingernews::Application.configure do
   config.action_controller.perform_caching = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
-  config.serve_static_assets = true
+  config.serve_static_assets = false
 
   # Compress JavaScripts and CSS
   config.assets.compress = true
@@ -47,6 +47,7 @@ Harbingernews::Application.configure do
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
+  config.assets.precompile += %w[ home/index.css ]
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
@@ -65,5 +66,4 @@ Harbingernews::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
   
-  config.assets.precompile = ['*.js', '*.css']
 end
