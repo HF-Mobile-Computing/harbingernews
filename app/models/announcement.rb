3a32ffc3ab20284e68596df48ba969e269f66f22
alexlafroscia <alex.lafroscia@gmail.com>
2013-04-04 17:48:45 -0400

    Re-ran "Annotate"

--- app/models/announcement.rb
+++ app/models/announcement.rb
@@ -1,3 +1,14 @@
 class Announcement < ActiveRecord::Base
   attr_accessible :a_or_b, :content
 end
+# == Schema Information
+#
+# Table name: announcements
+#
+#  id         :integer         not null, primary key
+#  a_or_b     :string(255)
+#  content    :text
+#  created_at :datetime        not null
+#  updated_at :datetime        not null
+#
+
--- spec/models/announcement_spec.rb
+++ spec/models/announcement_spec.rb
@@ -3,3 +3,14 @@ require 'spec_helper'
 describe Announcement do
   pending "add some examples to (or delete) #{__FILE__}"
 end
+# == Schema Information
+#
+# Table name: announcements
+#
+#  id         :integer         not null, primary key
+#  a_or_b     :string(255)
+#  content    :text
+#  created_at :datetime        not null
+#  updated_at :datetime        not null
+#
+
