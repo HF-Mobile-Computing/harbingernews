ec922268770f62287c865c3cc6e671502b23fe2b
alexlafroscia <alex.lafroscia@gmail.com>
2013-04-04 17:51:10 -0400

    Messing with adding Gravatars to user pages

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,10 @@
 module UsersHelper
+
+  def gravatar_for(user)
+    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    gravatar_url = "https://secure.gravatar.com/avatars/#{gravatar_id}.png"
+    image_tag(gravatar_url, alt: user.name, class:"gravatar")
+
+  end
+  
 end
