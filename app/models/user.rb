86475439429ab7dc80269e282d52656d75e7362d
alexlafroscia <alex.lafroscia@gmail.com>
2013-04-04 17:03:38 -0400

    Users with secure passwords

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,18 @@
 class User < ActiveRecord::Base
-  attr_accessible :email, :name
+  attr_accessible :email, :name, :password, :password_confirmation
+  has_secure_password
+  
+  before_save { |user| user.email = email.downcase }
   
   
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
   validates :name, presence: true, length: { maximum: 50 }, uniqueness: { case_sensitive: false }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence:     true, 
+                    format:       { with: VALID_EMAIL_REGEX }, 
+                    uniqueness:   { case_sensitive: false }
+  validates :password, presence: true, length: { minimum: 6 }
+  validates :password_confirmation, presence: true
+  
 end
 # == Schema Information
 #
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130404202708) do
+ActiveRecord::Schema.define(:version => 20130404204608) do
 
   create_table "announcements", :force => true do |t|
     t.string   "a_or_b"
@@ -34,8 +34,9 @@ ActiveRecord::Schema.define(:version => 20130404202708) do
   create_table "users", :force => true do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",      :null => false
+    t.datetime "updated_at",      :null => false
+    t.string   "password_digest"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
