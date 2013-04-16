86f66b11636055855a145dad7be0fd102c3b5551
Jake Dombrower <jake@jake-Aspire-V5-571P.(none)>
2013-04-16 16:41:16 -0400

    DELETE THIS STUFF

--- Old Stuff/assets/login/logout.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start();
-include 'sessions.php';
-logout();
-echo 'you\'ve been logged out.';
-?>
--- Old Stuff/datalogin.php
+++
@@ -1,8 +0,0 @@
-<?php
-$dbhost = '127.0.0.1';
-$dbname = 'harbinger';
-$dbuser = 'root';
-$dbpass = 'laFr0scia';
-$con = mysql_connect($dbhost, $dbuser, $dbpass) or die(mysql_error());
-mysql_select_db($dbname, $con) or die(mysql_error());
-?>
