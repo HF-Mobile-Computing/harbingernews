92c8d47832f091535b84536dbf3cd61482dc5594
Brian <bkelley620@gmail.com>
2013-04-16 13:32:15 -0400

    seed.rb  for clubs and announcement

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Announcement.create(content: "This is an announcement", a_or_b: "A")
+Club.create(name: "Art Club", category: "arts", content: "Content for the Art Club", slug: "art_club")
+Club.create(name: "Academic Club", category: "academic", content: "Content for the Academic Club", slug: "academic")
+Club.create(name: "Community Outreach", category: "community", content: "Content for the Community Outreach Club", slug:"community")
+Club.create(name: "Spanish Club", category: "cultural", content: "Content for the Spanish Club", slug: "spanish")
+Club.create(name: "Forensics Club", category: "science", content: "Content for the Forensics Club", slug: "forensics")
+Club.create(name: "Athletes Helping Athletes", category: "services", content: "Content for AHA", slug: "aha")
+
