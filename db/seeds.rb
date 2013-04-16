5eaea0145220954a21635976c6f9f47f6b08a78f
alexlafroscia <alex.lafroscia@gmail.com>
2013-04-16 13:33:54 -0400

    Added sport pre-population

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# Sports:
+#  => Sport(id: integer, slug: string, title: string, content: text, season: string, photo_path: string, created_at: datetime, updated_at: datetime, alert: text) 
+
+Sport.create(slug: "basketball", title: "Basketball", content: "This is the Basketball page.", season: "winter")
+Sport.create(slug: "football", title: "Football", content: "This is the Football page.", season: "fall")
+Sport.create(slug: "baseball", title: "Baseball", content: "This is the Baseball page.", season: "spring")
