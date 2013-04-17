7601470dfe41c1943be115b8025161153d6f60f8
alexlafroscia <alex.lafroscia@gmail.com>
2013-04-17 17:10:35 -0400

    Added new seeds for memos

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,16 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+# Annoucements
 Announcement.create(content: "This is an announcement", a_or_b: "A")
 
+# Memos
+Memo.create(title: "Test Memo 1", content: "This is the content for test memo one.")
+Memo.create(title: "Test Memo 2", content: "This is the content for test memo two.")
+Memo.create(title: "Test Memo 3", content: "This is the content for test memo three.")
+Memo.create(title: "Test Memo 4", content: "This is the content for test memo four.")
+
+# Clubs
 Club.create(name: "Art Club", category: "arts", content: "Content for the Art Club", slug: "art_club")
 Club.create(name: "Academic Club", category: "academic", content: "Content for the Academic Club", slug: "academic")
 Club.create(name: "Community Outreach", category: "community", content: "Content for the Community Outreach Club", slug:"community")
@@ -15,6 +23,7 @@ Club.create(name: "Spanish Club", category: "cultural", content: "Content for th
 Club.create(name: "Forensics Club", category: "science", content: "Content for the Forensics Club", slug: "forensics")
 Club.create(name: "Athletes Helping Athletes", category: "services", content: "Content for AHA", slug: "aha")
 
+# Sports
 Sport.create(slug: "basketball", title: "Basketball", content: "This is the Basketball page.", season: "winter")
 Sport.create(slug: "football", title: "Football", content: "This is the Football page.", season: "fall")
 Sport.create(slug: "baseball", title: "Baseball", content: "This is the Baseball page.", season: "spring")
