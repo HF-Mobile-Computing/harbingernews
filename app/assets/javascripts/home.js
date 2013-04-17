d2b39429602bf85fed1177265b3d525091c2428b
alexlafroscia <alex.lafroscia@gmail.com>
2013-04-17 17:10:52 -0400

    Got the animation working nicely with multiple sets of memos

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -3,16 +3,43 @@
 //= require_self
 
 $(function() {
+  // Add "active" to nav bar
+  $('#nav_home').addClass('active');
+  
+  // Constantly Check Page Width
+  
+  var thePage = {};
+  thePage.width = $(window).width();
+  $(window).resize(function(){
+      thePage.width = $(window).width();
+  });
+  
+  // Operate the slider for memos
+  // Note: All console.log statements can be removed before production
+  if (thePage.width > 979) {
+    slideAmt = 849;
+  }
+  counter = 0;
+  
+  console.log("counter = " + counter);
   $('#advance').click(function() {
-    console.log("Advance button clicked");
-    $('#slider').animate({
-      right: '+=50',
-    }, 100);
+    // console.log("Advance button clicked");
+    if (counter < 5) {
+      $('#slider').animate({
+        right: '+=' + slideAmt,
+      }, 100);
+      counter = counter + 1;
+    }
+    console.log("counter = " + counter);
   });
   $('#reverse').click(function() {
-    console.log("Reverse button clicked");
-    $('#slider').animate({
-      right: '-=50',
-    }, 100);
+    // console.log("Reverse button clicked");
+    if (counter > 0) {
+      $('#slider').animate({
+        right: '-=' + slideAmt,
+      }, 100);
+      counter = counter - 1;
+    }
+    console.log("counter = " + counter);
   });
 });
