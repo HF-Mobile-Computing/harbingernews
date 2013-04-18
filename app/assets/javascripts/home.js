b5cedccf007aad9568ab8ad7c66708f18313b298
alexlafroscia <alex.lafroscia@gmail.com>
2013-04-18 16:18:01 -0400

    Attempting to get the slider resetter to work

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -9,50 +9,63 @@ $(function() {
   // Constantly Check Page Width and set variables accordingly
   
   var thePage = {};
+  thePage.width = $(window).width();
   function setVars() {
-    thePage.width = $(window).width();
     if (thePage.width >= 980) {
       thePage.slideAmt = 849;
       thePage.counterAmt = 3;
-    } else if (thePage.width <= 979 && thePage.width >= 768) {
+    } else if (thePage.width <= 979 && thePage.width >= 670) {
       thePage.slideAmt = 566; 
       thePage.counterAmt = 5;
+    } else if (thePage.width < 670) {
+      thePage.slideAmt = 283;
+      thePage.counterAmt = 11;
     }
   }
   
+  thePage.counter = 0;
+  
   setVars();
   $(window).resize(function(){
+      thePage.width = $(window).width();
       setVars();
+      if (thePage.width == 980 || thePage.width == 670) {
+        console.log('reset of slider should take place: ' + thePage.width);
+        $('#slider').css('right', 0);
+        thePage.counter = 0;
+      }
   });
   
-  // Operate the slider for memos
-  // Note: All console.log statements can be removed before production
-  
-  counter = 0;
+  thePage.startSize = $(window).width();
+  $(window).resize(function(){
+      if (thePage.width == 980) {
+        console.log("%cStart size is set to " + thePage.width, "color: blue;");
+      }
+  });
   
   $('#advance').click(function() {
     // console.log("Advance button clicked");
-    if (counter < thePage.counterAmt) {
+    if (thePage.counter < thePage.counterAmt) {
       $('#slider').animate({
         right: '+=' + thePage.slideAmt,
       }, 100);
-      counter = counter + 1;
+      thePage.counter = thePage.counter + 1;
     }
     testLog();
   });
   $('#reverse').click(function() {
     // console.log("Reverse button clicked");
-    if (counter > 0) {
+    if (thePage.counter > 0) {
       $('#slider').animate({
         right: '-=' + thePage.slideAmt,
       }, 100);
-      counter = counter - 1;
+      thePage.counter = thePage.counter - 1;
     }
     testLog();
   });
   
   function testLog() {
-    console.log("counter = " + counter);
+    console.log("counter = " + thePage.counter);
     console.log("Counter Amt: " + thePage.counterAmt);
     console.log("Slide Amt: " + thePage.slideAmt);
   };
