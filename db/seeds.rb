b22290aa8f75ab36595b25066485b39221e25570
alexlafroscia <alex.lafroscia@gmail.com>
2013-04-18 16:18:10 -0400

    MOAR SEEDZ

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,6 +14,14 @@ Memo.create(title: "Test Memo 1", content: "This is the content for test memo on
 Memo.create(title: "Test Memo 2", content: "This is the content for test memo two.")
 Memo.create(title: "Test Memo 3", content: "This is the content for test memo three.")
 Memo.create(title: "Test Memo 4", content: "This is the content for test memo four.")
+Memo.create(title: "Test Memo 5", content: "This is the content for test memo five.")
+Memo.create(title: "Test Memo 6", content: "This is the content for test memo six.")
+Memo.create(title: "Test Memo 7", content: "This is the content for test memo seven.")
+Memo.create(title: "Test Memo 8", content: "This is the content for test memo eight.")
+Memo.create(title: "Test Memo 9", content: "This is the content for test memo nine.")
+Memo.create(title: "Test Memo 10", content: "This is the content for test memo ten.")
+Memo.create(title: "Test Memo 11", content: "This is the content for test memo eleven.")
+Memo.create(title: "Test Memo 12", content: "This is the content for test memo twelve.")
 
 # Clubs
 Club.create(name: "Art Club", category: "arts", content: "Content for the Art Club", slug: "art_club")
