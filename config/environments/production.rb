c069f206448f3a2a983ab58978271b358c4312f2
alexlafroscia <alex.lafroscia@gmail.com>
2013-05-10 08:52:55 -0400

    Possible fix for the undefined variable $red issue
    Plus, deleted some un needed javascript files

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -48,8 +48,7 @@ Harbingernews::Application.configure do
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
-  config.assets.precompile = ['*.js', '*.css', '*.png', '*.jpg']
-  config.assets.precompile += %w(rails_admin/rails_admin.css, rails_admin/rails_admin.js)
+  config.assets.precompile = %w(rails_admin/rails_admin.css rails_admin/rails_admin.js home/index.css home.js )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
