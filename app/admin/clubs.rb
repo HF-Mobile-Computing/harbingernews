c24bf0b20eab01b1813fda669bdc440b5c44bc56
alexlafroscia <alex.lafroscia@gmail.com>
2013-06-06 10:11:02 -0400

    Customizing the Clubs show page

--- app/admin/clubs.rb
+++ app/admin/clubs.rb
@@ -1,3 +1,15 @@
 ActiveAdmin.register Club do
   
+  filter :name
+  filter :category
+  filter :content
+  
+  index as: :table do
+    selectable_column
+    column :id
+    column :name
+    column :category
+    default_actions
+  end
+  
 end
