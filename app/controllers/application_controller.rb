ff1d6fcae4406d810064012c5256157fdb6013df
kelleyb <bkelley620@gmail.com>
2013-06-17 13:13:07 -0400

    Hopefully fix the "We're sorry, an error has occured" message.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  root_path = "/"
 
   ## keep user on same page after successful sign in or sign out
   def after_sign_out_path_for(resource_or_scope)
