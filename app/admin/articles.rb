6668144aa44550e6696c218d4d6ed8c535d12273
Jake Dombrower <jakedombrowerdroid@gmail.com>
2013-10-09 16:57:19 -0400

    Article Active Admin custom form

--- app/admin/articles.rb
+++ app/admin/articles.rb
@@ -1,3 +1,11 @@
 ActiveAdmin.register Article do
-
+form do |f|
+    f.inputs "Details" do
+      f.input :title
+      f.input :body, :as => :rich, :config => { :width => '76%', :height => '400px' }
+    end
+    f.inputs do
+      f.submit
+    end
+  end
 end
