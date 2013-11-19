f697943e2031dad193ab8e174843a8bdc1ef5075
Jake <jake@jake-Aspire-V5-571P.(none)>
2013-11-19 17:04:21 -0500

    article categories

--- app/admin/articles.rb
+++ app/admin/articles.rb
@@ -7,7 +7,7 @@ form do |f|
     f.inputs "Details" do
       f.input :title
       f.input :author
-      f.input :category, :as => :select, :collection => [["Review", "review"], ["No Category", ""]]
+      f.input :category, :as => :select, :collection => [["Review", "review"], ["Sport Article", "sports"], ["Club Article", "clubs"], ["No Category", ""]]
       f.input :body, :as => :rich, :config => { :width => '76%', :height => '400px' }
     end
     f.inputs do
