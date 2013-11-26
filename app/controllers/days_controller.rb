714572e28a5198ba50f17b3e0b1d1586d352ed38
Jake Dombrower <jake@Element.(none)>
2013-11-26 13:12:24 -0500

    XML builder for days

--- app/controllers/days_controller.rb
+++ app/controllers/days_controller.rb
@@ -4,7 +4,7 @@ class DaysController < ApplicationController
 		respond_to do |format|
 	        format.html # index.html.erb
 	        format.json { render json: @day }
-	        format.xml {render xml: @day }
+	        format.xml 
       	end
 	end
 end
---
+++ app/views/days/index.xml.builder
@@ -0,0 +1,10 @@
+xml.instruct!  
+  
+xml.day do  
+  
+	xml.aorb(@day.aorb)  
+	xml.created_at(@day.created_at)  
+	xml.updated_at(@day.updated_at)  
+	xml.id(@day.id)  
+  
+end  
