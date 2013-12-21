99e8a37c3ccbf23f9c3a756db741ea5a24c5cf4d
Brian <bkelley620@gmail.com>
2013-12-20 19:41:42 -0500

    Made a seed for the day. Also fixed something bothering me.

--- db/seeds.rb
+++ db/seeds.rb
@@ -56,7 +56,7 @@ end
 # Buses
 if Bus.count == 0
 	Bus.create(one: '1', two: '2', three: '3', four: '4', five: '5', six: '6', seven: '7', eight: '8', nine: '9', ten: '10', eleven: '11', twelve: '12', thirteen: '13', fourteen: '14', fifteen: '15', sixteen: '16', seventeen: '17', eighteen: '18', nineteen: '19', twenty: '20')
-	puts "Buses created"
+	puts "Created buses"
 else
 	puts "Buses already exist"
 end
@@ -81,6 +81,13 @@ if ClubAnnouncement.count == 0
 	puts "Created club announcement"
 else
 	puts "Club Announcement already exist"
+end
+
+if Day.count == 0
+	Day.create(aorb: "B")
+	puts "Created Day"
+else
+	puts "Day already exists"
 end
 
 puts "Seed complete!"
