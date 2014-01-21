492f4b5e75fdb5e8130ee382492938edb306e0ba
Jake Dombrower <jakedombrowerdroid@gmail.com>
2014-01-20 19:21:49 -0500

    Misc admin stuff

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -1,13 +1,21 @@
 ActiveAdmin.register_page "Dashboard" do
+sidebar :help do
+      ul do
+        h1 "Attention:"
+        h3 "All changes to this site are logged. Remember, Admin Access is a privledge. Only modify what you have direct permission to."
+      end
+    end
 
   menu :priority => 1, :label => proc{ I18n.t("active_admin.dashboard") }
 
   content :title => proc{ I18n.t("active_admin.dashboard") } do
+
   
     # Announcements
     columns do
       column do
         panel "Site History" do
+          
           table_for Version.order('id desc') do
             column :whodunnit 
             column :item_type
--- app/admin/notifications.rb
+++ app/admin/notifications.rb
@@ -1,3 +1,5 @@
 ActiveAdmin.register Notification do
-
+ action_item do
+      link_to "Send a Push with Parse", "http://www.parse.com/apps/hf-navigator--4/push_notifications/new"
+    end
 end
