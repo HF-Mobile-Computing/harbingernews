64f3837b71d090b06ce42772af51425778c536d8
jakedombrower <jakedombrowerdroid@gmail.com>
2014-01-31 15:53:53 -0500

    Fixed botched migration

--- db/migrate/20140131201931_add_pdf_to_print_issues.rb
+++ db/migrate/20140131201931_add_pdf_to_print_issues.rb
@@ -1,11 +1,9 @@
 class AddPdfToPrintIssues < ActiveRecord::Migration
   def self.up
     add_attachment :print_issues, :pdf
-    remove_column :print_issues, :link
   end
 
   def self.down
     remove_attachment :print_issues, :pdf
-    add_column :print_issues, :link
   end
 end
