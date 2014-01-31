a5559c0e67f45488d66b671b1c8aa618203e47d3
jakedombrower <jakedombrowerdroid@gmail.com>
2014-01-31 15:45:09 -0500

    remove_column

--- db/migrate/20140131201931_add_pdf_to_print_issues.rb
+++ db/migrate/20140131201931_add_pdf_to_print_issues.rb
@@ -1,11 +1,11 @@
 class AddPdfToPrintIssues < ActiveRecord::Migration
   def self.up
     add_attachment :print_issues, :pdf
-    remove_attachment :print_issues, :link
+    remove_column :print_issues, :link
   end
 
   def self.down
     remove_attachment :print_issues, :pdf
-    add_attachment :print_issues, :link
+    add_column :print_issues, :link
   end
 end
