aec9479276b340b7779351bae51ecc2e4e6b974e
jakedombrower <jakedombrowerdroid@gmail.com>
2014-03-19 22:46:01 -0400

    Club photo thumbnails

--- app/models/club_photo.rb
+++ app/models/club_photo.rb
@@ -1,6 +1,6 @@
 class ClubPhoto < ActiveRecord::Base
    attr_accessible :photo, :club_id
-   has_attached_file :photo
+   has_attached_file :photo, styles: { thumbnail: '100x100>' }
    belongs_to :club
    has_paper_trail
 end
