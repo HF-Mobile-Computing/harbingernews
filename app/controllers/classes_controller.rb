5c0ec163a5e847b4a205a6b04de1ee69686d9894
jakedombrower <jakedombrowerdroid@gmail.com>
2014-04-29 12:47:40 -0400

    fixed header for classes

--- app/controllers/classes_controller.rb
+++ app/controllers/classes_controller.rb
@@ -14,5 +14,13 @@ class ClassesController < ApplicationController
 
   def show
   	@class = AcademicClass.find_by_slug(params[:slug])
+    @english = AcademicClass.where(:category => "english")
+    @social_studies = AcademicClass.where(:category => "social_studies")
+    @science = AcademicClass.where(:category => "science")
+    @math = AcademicClass.where(:category => "math")
+    @foreign_language = AcademicClass.where(:category => "foreign_language")
+    @arts_and_music = AcademicClass.where(:category => "arts_and_music")
+    @grades = AcademicClass.where(:category => "grades")
+
   end
 end
